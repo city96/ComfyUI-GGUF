8e898fad4caab59bf4144e0cf11978b893de7e54
RandomGitUser321 <ruinerx@gmail.com>
2024-11-06 08:04:04 -0500

    Add mochi to CLIPLoaderGGUF options (#154)
    * Add mochi to CLIPLoaderGGUF options
    
    ComfyUI implemented it today, so this should be all that's needed to use the T5 GGUF with it
    
    * Update nodes.py

--- nodes.py
+++ nodes.py
@@ -287,6 +287,7 @@ clip_name_dict = {
     "sdxl": comfy.sd.CLIPType.STABLE_DIFFUSION,
     "sd3": comfy.sd.CLIPType.SD3,
     "flux": comfy.sd.CLIPType.FLUX,
+    "mochi": getattr(comfy.sd.CLIPType, "MOCHI", None),
 }
 
 class CLIPLoaderGGUF:
@@ -295,7 +296,7 @@ class CLIPLoaderGGUF:
         return {
             "required": {
                 "clip_name": (s.get_filename_list(),),
-                "type": (["stable_diffusion", "stable_cascade", "sd3", "stable_audio"],),
+                "type": (["stable_diffusion", "stable_cascade", "sd3", "stable_audio", "mochi"],),
             }
         }
 
