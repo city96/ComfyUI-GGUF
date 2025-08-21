0f4b15292a24728085634633a7d38b92a8af77ce
thipokKub <thipok.tham@gmail.com>
2025-08-22 01:57:50 +0700

    Draft add hidden mmproj

--- __init__.py
+++ __init__.py
@@ -7,3 +7,6 @@ else:
     from .nodes import NODE_CLASS_MAPPINGS
     NODE_DISPLAY_NAME_MAPPINGS = {k:v.TITLE for k,v in NODE_CLASS_MAPPINGS.items()}
     __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
+
+WEB_DIRECTORY = "./js"
---
+++ js/clip_loader.js
@@ -0,0 +1,42 @@
+import { app } from "../../scripts/app.js"
+
+app.registerExtension({
+	name: "ComfyUI_GGUF.clip_loader_gguf",
+	async beforeRegisterNodeDef(nodeType, nodeData, app) {
+		if (nodeData.name === "CLIPLoaderGGUF") {
+			const onNodeCreated = nodeType.prototype.onNodeCreated;
+
+			nodeType.prototype.onNodeCreated = function () {
+				onNodeCreated?.apply(this, arguments);
+				const node = this;
+
+				const typeWidget = node.widgets.find((w) => w.name === "type");
+
+				const updateWidgets = (type) => {
+					const existingWidget = node.widgets.find((w) => w.name === "mmproj_path");
+					if (existingWidget) {
+						node.widgets.splice(node.widgets.indexOf(existingWidget), 1);
+					}
+
+					if (type === "qwen_image_edit") {
+						const newWidget = node.addWidget(
+							"string",
+							"mmproj_path",
+							"",
+							(value) => {},
+						);
+					}
+					const newSize = node.computeSize();
+					node.size = newSize;
+					app.graph.setDirtyCanvas(true, true);
+				};
+				typeWidget.callback = (value) => {
+					updateWidgets(value);
+				};
+				setTimeout(() => {
+					updateWidgets(typeWidget.value);
+				}, 1);
+			};
+		}
+	},
+});
